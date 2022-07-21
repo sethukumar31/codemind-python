2be92e9e963fe4222222fafbf43d9da05b6191f9
sethu kumar kongu <97594579+sethukumar31@users.noreply.github.com>
2022-07-21 11:23:35 +0530

    n=int(input()) t=n rev=0 while(n):     d=n%10     rev=rev*10+d     n=n//10 if t==rev:     print('True') else:     print('False')

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,11 @@
 n=int(input())
 t=n
 rev=0
-while n:
+while(n):
     d=n%10
     rev=rev*10+d
     n=n//10
-if rev==t:
-    print("True")
+if t==rev:
+    print('True')
 else:
-    print("False")
+    print('False')
