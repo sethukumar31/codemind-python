87b86a9358437dff96651e297cbe3601be952d19
sethu kumar kongu <97594579+sethukumar31@users.noreply.github.com>
2022-07-21 23:55:06 +0530

    n=int(input()) if n<0:     print('-',end="")     print(int(str(abs(n))[::-1])) else:     print(int(str(n)[::-1]))

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,14 +1,6 @@
 n=int(input())
-sum=0
-f=0
 if n<0:
-    f=1
-    n=abs(n)
-while n!=0:
-    r=n%10
-    sum=sum*10+r
-    n=n//10
-if f==0:
-    print(sum)
+    print('-',end="")
+    print(int(str(abs(n))[::-1]))
 else:
-    print("-%d" %sum)
+    print(int(str(n)[::-1]))
