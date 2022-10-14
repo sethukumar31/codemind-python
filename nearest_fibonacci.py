52a2003f8478ff42372e493d3d50ba4823575b2b
sethu kumar kongu <97594579+sethukumar31@users.noreply.github.com>
2022-10-14 22:15:25 +0530

    n=int(input()) a=0 b=1 while(True):     c=a+b     if(c>n):         x=b         y=c         break     a=b     b=c r=abs(x-n) s=abs(y-n) if(r>s):     print(y) elif(r==s):     print(x,y) else:     print(x)

---
+++ nearest_fibonacci.py
@@ -0,0 +1,19 @@
+n=int(input())
+a=0
+b=1
+while(True):
+    c=a+b
+    if(c>n):
+        x=b
+        y=c
+        break
+    a=b
+    b=c
+r=abs(x-n)
+s=abs(y-n)
+if(r>s):
+    print(y)
+elif(r==s):
+    print(x,y)
+else:
+    print(x)
