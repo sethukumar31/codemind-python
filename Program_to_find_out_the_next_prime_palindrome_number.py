808bebd160ed8644a0edeb9b43fcee1e4f605274
sethu kumar kongu <97594579+sethukumar31@users.noreply.github.com>
2022-10-15 23:22:12 +0530

    def prime(n):     c=0     for i in range(2,(n//2)+1):         if(n%i==0):             c+=1     if(c==0):         return 1 def pali(n):     if(str(n)==(str(n)[::-1])):         return 1     else:         return 0 n=int(input()) for i in range(n+1,n*n):     if(prime(i)==1 and pali(i)==1):         print(i)         break

---
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -0,0 +1,17 @@
+def prime(n):
+    c=0
+    for i in range(2,(n//2)+1):
+        if(n%i==0):
+            c+=1
+    if(c==0):
+        return 1
+def pali(n):
+    if(str(n)==(str(n)[::-1])):
+        return 1
+    else:
+        return 0
+n=int(input())
+for i in range(n+1,n*n):
+    if(prime(i)==1 and pali(i)==1):
+        print(i)
+        break
